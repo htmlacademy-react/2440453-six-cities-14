e5de4d60fdd86cf5aa69252f90db12060f5e1cea
Пятибратова Софья <SofjaEsenina@yandex.ru>
2023-10-28 11:40:14 +0300

    2.2 Добавила типы

---
+++ src/types/offer.ts
@@ -0,0 +1,40 @@
+type TLocation = {
+    latitude: number;
+    longitude: number;
+    zoom: number;
+}
+
+type TCity = {
+  location: TLocation;
+  name: string;
+}
+
+type THost = {
+  avatarUrl: string;
+  id: number;
+  isPro: boolean;
+  name: string;
+}
+
+export type TOffer = {
+  title: string;
+  bedrooms?: number;
+  location?: TLocation;
+  description?: string;
+  goods?: [string];
+  host?: THost;
+  id?: number;
+  images?: [string];
+  isFavorite: boolean;
+  isPremium: boolean;
+  maxAdults?: number;
+  previewImage: string;
+  price: number;
+  rating: number;
+  type: string;
+  city?: TCity;
+}
+
+export type TOfferList = TOffer[];
+
+
