d62af9d5efdd35d6c0ee4e152e32eab816635f4c
Пятибратова Софья <SofjaEsenina@yandex.ru>
2023-10-30 01:34:20 +0300

    2.4 Добавила линк на главную на страницу 404

--- src/pages/page-not-found/page-not-found.tsx
+++ src/pages/page-not-found/page-not-found.tsx
@@ -1,12 +1,17 @@
+import { Link } from 'react-router-dom';
 import LogoFooter from '../../components/logo-footer/logo-footer';
+import { ROUTE_LIST } from '../../const';
 
-function PageNotFound(): JSX.Element {
+function PageNotFound(): JSX.Element {//TODO навигацию с логином сюда тоже надо
   return (
     <div className="page page--gray page--login">
       <main className="page__main page__main--login">
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">PAGE NOT FOUND</h1>
+            <Link to={ROUTE_LIST.Root}>
+              <h2>Вернуться на главную</h2>
+            </Link>
           </section>
           <LogoFooter/>
         </div>
