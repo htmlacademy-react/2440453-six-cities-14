10d570a692ec3c13cb4da8a09aff44fbf7f94960
Пятибратова Софья <SofjaEsenina@yandex.ru>
2023-10-30 08:21:19 +0300

    2.7 Обновила путь к избранному

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -6,6 +6,8 @@ import LoginPage from '../../pages/login-page/login-page';
 import FavoritesPage from '../../pages/favorites-page/favorites-page';
 import OfferPage from '../../pages/offer-page/offer-page';
 import PageNotFound from '../../pages/page-not-found/page-not-found';
+import PrivateRoute from '../private-route/private-route';
+import { AuthorizationStatus } from '../../auth.enum';
 
 type TAppProps = {
   offerCount: number;
@@ -18,7 +20,7 @@ function App ({offerCount, offersList} : TAppProps): JSX.Element {
       <Routes>
         <Route path={ROUTE_LIST.Root} element={<MainPage offerCount={offerCount} offersList={offersList}/>}/>
         <Route path={ROUTE_LIST.Login} element={<LoginPage/>}/>
-        <Route path={ROUTE_LIST.Favourites} element={<FavoritesPage/>}/>
+        <Route path={ROUTE_LIST.Favourites} element={<PrivateRoute authStatus={AuthorizationStatus.NoAuth}><FavoritesPage/></PrivateRoute> }/>
         <Route path={ROUTE_LIST.Offer} element={<OfferPage/>}/>
         <Route path={ROUTE_LIST.Unknown} element={<PageNotFound/>}/>
       </Routes>
--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -1,6 +1,15 @@
-function PrivateRoute(): JSX.Element {
+import {Navigate} from 'react-router-dom';
+import { AuthorizationStatus } from '../../auth.enum';
+import { ROUTE_LIST } from '../../const';
+
+type PrivateRouteProps = {
+  children: JSX.Element;
+  authStatus: AuthorizationStatus;
+}
+
+function PrivateRoute({authStatus, children}: PrivateRouteProps): JSX.Element {
   return (
-    <h1>Тут будет компонент</h1>
+    authStatus === AuthorizationStatus.Auth ? children : <Navigate to={ROUTE_LIST.Root}/>
   );
 }
 
