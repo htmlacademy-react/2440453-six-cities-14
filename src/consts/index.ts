78eb5d8a62c4fa73cf12f9984280acb4bf9a4b6a
Пятибратова Софья <SofjaEsenina@yandex.ru>
2023-11-14 23:00:14 +0300

    5.1 Добавила константу для размера галлереи

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,10 +1,12 @@
 import { AuthorizationStatus } from './auth.enum';
 import { RATING } from './raiting';
 import { ROUTE_LIST, CITY_LIST } from './const';
+import { MAX_GALLERY_SIZE } from './max-gallery-size';
 
 export {
   AuthorizationStatus,
   RATING,
   ROUTE_LIST,
   CITY_LIST,
+  MAX_GALLERY_SIZE,
 };
