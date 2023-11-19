521df14fa9fef790595f9df2e79aa06fd7d01693
Пятибратова Софья <SofjaEsenina@yandex.ru>
2023-11-19 19:45:20 +0300

    5.4 добавила дефолтный город и константы для иконок

--- src/consts/const.ts
+++ src/consts/const.ts
@@ -1,3 +1,5 @@
+import { TCity } from '../types';
+
 const ROUTE_LIST = {
   'Root' : '/',
   'Unknown' : '*',
@@ -9,8 +11,21 @@ const ROUTE_LIST = {
 
 const CITY_LIST = ['Amsterdam', 'Brussels', 'Cologne', 'Dusseldorf', 'Hamburg', 'Paris'] as const;
 
+const DEFAULT_CITY: TCity = {
+  name: 'Amsterdam',
+  location: {
+    latitude: 52.37454,
+    longitude: 4.897976,
+    zoom: 13
+  },
+} as const;
+
 export {
   ROUTE_LIST,
   CITY_LIST,
+  DEFAULT_CITY,
 };
 
+export const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+
+export const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
--- src/consts/index.ts
+++ src/consts/index.ts
@@ -1,6 +1,6 @@
 import { AuthorizationStatus } from './auth.enum';
 import { RATING } from './raiting';
-import { ROUTE_LIST, CITY_LIST } from './const';
+import { ROUTE_LIST, CITY_LIST, DEFAULT_CITY, URL_MARKER_DEFAULT, URL_MARKER_CURRENT } from './const';
 import { MAX_GALLERY_SIZE } from './max-gallery-size';
 
 export {
@@ -9,4 +9,7 @@ export {
   ROUTE_LIST,
   CITY_LIST,
   MAX_GALLERY_SIZE,
+  DEFAULT_CITY,
+  URL_MARKER_DEFAULT,
+  URL_MARKER_CURRENT,
 };
