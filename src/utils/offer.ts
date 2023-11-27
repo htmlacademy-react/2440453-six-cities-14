294b1babe20553446ebcb9647563ce2939533d79
Пятибратова Софья <SofjaEsenina@yandex.ru>
2023-11-27 17:42:11 +0300

    6.7 Функция для сортировки

--- src/utils/offer.ts
+++ src/utils/offer.ts
@@ -1,3 +1,5 @@
+import { TOfferList, TSortItem } from '../types';
+
 function calcHiddenPremiumClass(isPremium: boolean, className: string): string {
   const classHiddenPremium = !isPremium ? ' visually-hidden' : '';
   return (`${className}${classHiddenPremium}`);
@@ -7,4 +9,17 @@ function calcRaitingPersent(rating:number): string {
   return (`${Math.round(+rating * 100 / 5)}%`);
 }
 
-export { calcHiddenPremiumClass, calcRaitingPersent };
+function sortOffersList(offersList: TOfferList, sortItem: TSortItem): TOfferList {
+  switch (sortItem) {
+    case 'Popular':
+      return offersList;
+    case 'Price: high to low':
+      return offersList.sort((a, b) => b.price - a.price);
+    case 'Price: low to high':
+      return offersList.sort((a, b) => -(b.price - a.price));
+    case 'Top rated first':
+      return offersList.sort((a, b) => b.rating - a.rating);
+  }
+}
+
+export { calcHiddenPremiumClass, calcRaitingPersent, sortOffersList };
