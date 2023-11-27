335a342c43929d0d8e77e57a6746a2cdab8b022e
Пятибратова Софья <SofjaEsenina@yandex.ru>
2023-11-27 17:43:34 +0300

    6.8 Добавила сортировку

--- src/components/offer-card-list/offer-card-list.tsx
+++ src/components/offer-card-list/offer-card-list.tsx
@@ -1,6 +1,10 @@
 import { TCityName, TOfferList } from '../../types';
 import SortBlock from '../sort-block/sort-block';
 import OfferCard from '../offer-card/offer-card';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import { changeSortItem } from '../../store/action';
+import { TSortItem } from '../../types';
+import { sortOffersList } from '../../utils/offer';
 
 type TOfferListProps = {
   offersList: TOfferList;
@@ -10,13 +14,18 @@ type TOfferListProps = {
 }
 
 function OfferCardList ({offersList, city, handleMouseEnter, handleMouseLeave}: TOfferListProps): JSX.Element {
-  const offers = offersList.map((item) => <OfferCard offer={item} key={item.id} onMouseEnter={() => handleMouseEnter(item.id)} onMouseLeave={handleMouseLeave}/>);
+  const dispatch = useAppDispatch();
+  const handleItemClick = (sortItem: TSortItem) => {
+    dispatch(changeSortItem({item: sortItem}));
+  };
+  const selectedSortItem = useAppSelector((state) => state.sortItem);
+  const offers = sortOffersList(offersList, selectedSortItem).map((item) => <OfferCard offer={item} key={item.id} onMouseEnter={() => handleMouseEnter(item.id)} onMouseLeave={handleMouseLeave}/>);
   const listHeader = offersList.length === 0 ? 'No places to stay available' : `${offersList.length} places to stay in ${city}`;
   return (
     <section className="cities__places places">
       <h2 className="visually-hidden">Places</h2>
       <b className="places__found">{listHeader}</b>
-      {offersList.length > 0 && <SortBlock/>}
+      {offersList.length > 0 && <SortBlock selectedSortItem={selectedSortItem} onClick={handleItemClick}/>}
       <div className="cities__places-list places__list tabs__content">
         {offers}
       </div>
--- src/components/sort-block/sort-block.tsx
+++ src/components/sort-block/sort-block.tsx
@@ -1,18 +1,27 @@
+import { SORTING_TYPES } from '../../consts';
+import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
+import { changeSortItem } from '../../store/action';
+import { TSortItem } from '../../types';
+import SortItem from './sort-item';
+
 function SortBlock() : JSX.Element {
+  const dispatch = useAppDispatch();
+  const handleItemClick = (sortItem: TSortItem) => {
+    dispatch(changeSortItem({item: sortItem}));
+  };
+  const selectedSortItem = useAppSelector((state) => state.sortItem);
+  const sortItems = SORTING_TYPES.map((item, i) => <SortItem name={item} tabIndex={i} isSelected={item === selectedSortItem} key={item} onClick={handleItemClick}/>);
   return (
     <form className="places__sorting" action="#" method="get">
-      <span className="places__sorting-caption">Sort by</span>
+      <span className="places__sorting-caption">Sort by </span>
       <span className="places__sorting-type" tabIndex={0}>
-              Popular
+        {selectedSortItem}
         <svg className="places__sorting-arrow" width="7" height="4">
           <use xlinkHref="#icon-arrow-select"></use>
         </svg>
       </span>
       <ul className="places__options places__options--custom places__options--opened">
-        <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-        <li className="places__option" tabIndex={0}>Price: low to high</li>
-        <li className="places__option" tabIndex={0}>Price: high to low</li>
-        <li className="places__option" tabIndex={0}>Top rated first</li>
+        {sortItems}
       </ul>
     </form>
   );
