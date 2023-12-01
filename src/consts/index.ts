b3a45e8bdbe18c9380953c5cf4331e61e6c3727d
Пятибратова Софья <SofjaEsenina@yandex.ru>
2023-12-01 07:31:45 +0300

    7.1 Константы для бэкэнда

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -2,6 +2,7 @@ import { AuthorizationStatus } from './auth.enum';
 import { RATING } from './raiting';
 import { ROUTE_LIST, CITY_LIST, CITIES_LIST, URL_MARKER_DEFAULT, URL_MARKER_CURRENT, SORTING_TYPES } from './const';
 import { MAX_GALLERY_SIZE } from './max-gallery-size';
+import { BACKEND_URL, TIMEOUT } from './backend';
 
 export {
   AuthorizationStatus,
@@ -13,4 +14,6 @@ export {
   URL_MARKER_DEFAULT,
   URL_MARKER_CURRENT,
   SORTING_TYPES,
+  TIMEOUT,
+  BACKEND_URL,
 };
