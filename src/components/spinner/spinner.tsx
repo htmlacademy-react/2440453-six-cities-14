fe16b8967cb60ce27f85677725bbacc7ccdd1306
Пятибратова Софья <SofjaEsenina@yandex.ru>
2023-12-03 23:40:09 +0300

    7.2. Компонент для загрузки данных

---
+++ src/components/spinner/spinner.tsx
@@ -0,0 +1,11 @@
+function Spinner() {
+  return (
+    <div className="cities">
+      <div className="cities__places-container container">
+        <h2>Данные загружаются</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
