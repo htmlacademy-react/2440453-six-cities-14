22cbddbe1fb6cc8921331c262e24d8a64d6a283d
Пятибратова Софья <SofjaEsenina@yandex.ru>
2023-12-03 23:41:15 +0300

    7.3. Обновили хранилище

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,9 +1,12 @@
 import { createAction } from '@reduxjs/toolkit';
-import { TCityName, TSortItem } from '../types';
+import { TCityName, TOfferList, TSortItem } from '../types';
 
 export const selectCity = createAction<{city: TCityName}>('city/selection');
 
-export const fillOffers = createAction('offers/fill');
+export const fillOffers = createAction<TOfferList>('offers/fill');
 
 export const changeSortItem = createAction<{item: TSortItem}>('sort/change');
 
+export const setOffersLoadedStatus = createAction<boolean>('offers/loaded');
+
+export const setError = createAction<string | null>('app/error');
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,13 +1,13 @@
 import { createReducer } from '@reduxjs/toolkit';
 import { CITY_LIST, SORTING_TYPES } from '../consts';
-import { changeSortItem, fillOffers, selectCity } from './action';
+import { changeSortItem, fillOffers, selectCity, setOffersLoadedStatus } from './action';
 import { TState } from '../types';
-import OFFERS_LIST from '../mocks/offers';
 
 const initialState: TState = {
   city: CITY_LIST[0],
   offers: [],
   sortItem: SORTING_TYPES[0],
+  isOffersLoaded: false,
 };
 
 
@@ -15,11 +15,14 @@ const reducer = createReducer(initialState, (builder) => {
   builder.addCase(selectCity, (state, action) => {
     state.city = action.payload.city;
   });
-  builder.addCase(fillOffers, (state) => {
-    state.offers = OFFERS_LIST;
+  builder.addCase(fillOffers, (state, action) => {
+    state.offers = action.payload;
   });
   builder.addCase(changeSortItem, (state, action) => {
     state.sortItem = action.payload.item;
+  });
+  builder.addCase(setOffersLoadedStatus, (state, action) => {
+    state.isOffersLoaded = action.payload;
   });
 }
 );
